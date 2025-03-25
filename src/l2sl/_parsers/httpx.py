3d39ffd78c3f99c38caf97ae438870a47ba5ed3e
Philip Meier <github.pmeier@posteo.de>
2025-03-25 15:12:29 +0100

    remove regexp parser for simple cases

--- src/l2sl/_parsers/httpx.py
+++ src/l2sl/_parsers/httpx.py
@@ -1,16 +1,8 @@
 import logging
 from typing import Any
 
-from ._core import register_builtin_parser
-from ._regexp import RegexpEventParser
 
-httpx = register_builtin_parser("httpx", RegexpEventParser())
-
-
-@httpx.register_event_handler(r".*")
-def server_started_process(
-    groups: dict[str, str], record: logging.LogRecord
-) -> tuple[str, dict[str, Any]]:
+def httpx(event: str, record: logging.LogRecord) -> tuple[str, dict[str, Any]]:
     assert record.args is not None
     method, url, protocol, status_code, _ = record.args
     return "request", {
