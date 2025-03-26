0cff21fc8311131378ec1a491198dcaa268f4d5b
Philip Meier <github.pmeier@posteo.de>
2025-03-26 12:16:38 +0100

    improve debug

--- src/l2sl/_parsers/panel.py
+++ src/l2sl/_parsers/panel.py
@@ -12,3 +12,17 @@ def session(
     groups: dict[str, str], record: logging.LogRecord
 ) -> tuple[str, dict[str, Any]]:
     return groups.pop("event"), groups
+
+
+panel_viewable = register_builtin_parser(RegexpEventParser(), logger="panel.viewable")
+
+
+@panel_viewable.register_event_handler(
+    r"Session \d+ (?P<event>(received|finished processing) events)"
+)
+def handler(
+    groups: dict[str, str], record: logging.LogRecord
+) -> tuple[str, dict[str, Any]]:
+    assert record.args is not None
+    session_id, events = record.args
+    return groups["event"], {"sesion_id": session_id, "events": events}
