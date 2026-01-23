045b68bd7c67a9b53580830bf4a5a3f292a1962f
Philip Meier <github.pmeier@posteo.de>
2026-01-23 14:07:40 +0000

    remove record filter (#14)

--- src/l2sl/_forward.py
+++ src/l2sl/_forward.py
@@ -5,28 +5,22 @@ import logging.config
 
 import structlog
 
-from ._filter import RecordFilter, SimpleRecordFilter
-
 
 class _RecordForwarder(logging.Handler):
-    def __init__(self, *, record_filter: RecordFilter) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self._record_filter = record_filter
         self._logger = structlog.get_logger()
 
     def emit(self, record: logging.LogRecord) -> None:
-        if self._record_filter(record):
-            self._logger.log(
-                record.levelno,
-                record.msg,
-                *record.args,
-                record=record,
-            )
+        self._logger.log(
+            record.levelno,
+            record.msg,
+            *record.args,
+            record=record,
+        )
 
 
-def configure_stdlib_log_forwarding(
-    *, record_filter: RecordFilter = SimpleRecordFilter.default(level="info")
-) -> None:
+def configure_stdlib_log_forwarding() -> None:
     logging.config.dictConfig(
         {
             "version": 1,
@@ -34,7 +28,6 @@ def configure_stdlib_log_forwarding(
             "handlers": {
                 "structlog": {
                     "class": "l2sl._forward._RecordForwarder",
-                    "record_filter": record_filter,
                 }
             },
             "loggers": {"root": {"level": "NOTSET", "handlers": ["structlog"]}},
