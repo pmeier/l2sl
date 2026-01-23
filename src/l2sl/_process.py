fce0dbd5ef35c566f6a753a4703897a26cf027d7
Philip Meier <github.pmeier@posteo.de>
2026-01-23 15:22:26 +0100

    prevent parsing errors from raising exceptions

--- src/l2sl/_process.py
+++ src/l2sl/_process.py
@@ -1,8 +1,11 @@
 __all__ = ["StdlibRecordParser"]
 
 import logging
+import uuid
 from collections.abc import Mapping
-from typing import Any
+from typing import Any, cast
+
+import structlog
 
 from ._parsers import Parser, builtin_parsers, default_fallback_parser
 from ._select import LoggerSelector
@@ -14,7 +17,6 @@ class StdlibRecordParser:
         *,
         parsers: dict[str, Parser] | None = None,
         fallback: Parser = default_fallback_parser,
-        include_logger_name: bool | str = True,
     ) -> None:
         if parsers is None:
             parsers = builtin_parsers()
@@ -23,9 +25,7 @@ class StdlibRecordParser:
         self._fallback = fallback
         self._logger_selector = LoggerSelector(self._parsers.keys())
 
-        if not isinstance(include_logger_name, str):
-            include_logger_name = "logger" if include_logger_name else ""
-        self._include_logger_name = include_logger_name
+        self._logger = structlog.get_logger()
 
     def __call__(
         self, logger: Any, level: str, event_dict: Mapping[str, Any]
@@ -36,6 +36,8 @@ class StdlibRecordParser:
             return event_dict
 
         logger = record.name
+        event_dict["logger"] = logger
+
         selected_logger = self._logger_selector(logger)
         parser = (
             self._parsers[selected_logger]
@@ -43,12 +45,22 @@ class StdlibRecordParser:
             else self._fallback
         )
 
-        event, values = parser(event_dict.pop("event"), record)
+        event = event_dict.pop("event")
+        try:
+            event, values = parser(event, record)
+        except Exception as exc_info:
+            event, values = self._fallback(event, record)
+            l2sl_error_id = cast(dict[str, Any], values)["l2sl_error_id"] = str(
+                uuid.uuid4()
+            )
+            self._logger.error(
+                "failed to parse event",
+                logger="l2sl",
+                exc_info=exc_info,
+                l2sl_error_id=l2sl_error_id,
+            )
 
         event_dict["event"] = event
         event_dict.update(values)
-
-        if self._include_logger_name:
-            event_dict[self._include_logger_name] = logger
 
         return event_dict
